c348fcf59135e999ba74a0469ce6846a84516ac6
crisbelagui <leidy072003@gmail.com>
2023-11-23 14:03:34 -0500

    login
    se agregó imagen del login

--- proyectoencuestion/front-end/src/components/vistalogin.js
+++ proyectoencuestion/front-end/src/components/vistalogin.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {} from '@fortawesome/free-solid-svg-icons';
-import foto from '../assets/img/foto.png';
+import foto from './img/foto.png';
 import "../styles/login.css"; 
 
 
