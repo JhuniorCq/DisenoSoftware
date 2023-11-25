4778d1763107535b5c81338369aad12da4a983f5
JhuniorCq <holiver.ccora.quispe2@gmail.com>
2023-11-24 23:17:27 -0500

    La fe?

--- proyectoencuestion/back-end/src/db.js
+++ proyectoencuestion/back-end/src/db.js
@@ -2,12 +2,21 @@ const {Pool} = require('pg');
 const {db} = require('./config');
 
 const pool = new Pool({
-    user: db.user,
-    password: db.password,
-    host: db.host,
-    port: db.port,
-    database: db.database,
+    user: "modulomarketing_user",
+    password: "HVpxtC4kWQWF7r2Qi4LSbg2RTKs1i1oD",
+    host: "dpg-cl1gju0p2gis73f3v5ug-a.oregon-postgres.render.com",
+    port: 5432,
+    database: "modulomarketing",
     ssl: true
 });
+
+// const pool = new Pool({
+//     user: db.user,
+//     password: db.password,
+//     host: db.host,
+//     port: db.port,
+//     database: db.database,
+//     ssl: true
+// });
 
 module.exports = pool;
