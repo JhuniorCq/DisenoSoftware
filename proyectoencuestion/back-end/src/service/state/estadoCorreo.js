cabd88f4ab180430375663bf25271e663aa1c28b
JhuniorCq <holiver.ccora.quispe2@gmail.com>
2023-11-30 18:50:15 -0500

    Envío de correos con fecha de envío superior a la fecha actual -> REALIZADO

--- proyectoencuestion/back-end/src/service/state/estadoCorreo.js
+++ proyectoencuestion/back-end/src/service/state/estadoCorreo.js
@@ -1,12 +1,18 @@
-const transporter = require('../../nodemailer'); // Importar el transporter configurado
 const {enviarCorreosNodemailer} = require('../../enviarCorreosNodemailer')
 const {ClienteRepository} = require('../../repository/clienteRepository')
 const clienteRepository = new ClienteRepository();
-// const { differenceInDays, differenceInMinutes, differenceInSeconds } = require('date-fns');
 
-//Estado Programado
-class EstadoProgramado {
+class EstadoCorreo {
+    enviarCorreo(instanciaCorreo) {
+        throw new Error("Método abstracto. Debe ser implementado por clases derivadas.");
+    }
+}
+
+class EstadoProgramado extends EstadoCorreo {
     async enviarCorreo(instanciaCorreo) {
+
+        instanciaCorreo.transicionEstadoParticipante(instanciaCorreo.campana_id, 'Programado');
+        instanciaCorreo.estado = 'Programado'
 
         const fecha_actual_Date = new Date();
 
@@ -16,21 +22,16 @@ class EstadoProgramado {
         const hora_envio = instanciaCorreo.hora;
 
         const calcularDiferenciaHora = () => {
-            const [horaE, minutoE, segundoE] = hora_envio.split(":").map(Number);
-            const [horaA, minutoA, segundoA] = hora_actual.split(":").map(Number);
 
-            const diferenciaHoras = Math.abs(horaE - horaA)*60*60*1000;//PONERLO EN MILISEGUNDOS
-            const diferenciaMinutos = Math.abs(minutoE - minutoA)*60*1000;
-            const diferenciaSegundos = Math.abs(segundoE - segundoA)*1000;
+            //CONVIERTO A DATE, PERO PONIENDOLE CUALQUIER FECHA, YA QUE ESTAS SE ELIMINARÁN AL RESTARLAS
+            const horaActualDATE = new Date(`2023-11-30T${hora_actual}`);
+            const horaEnvioDATE = new Date(`2023-11-30T${hora_envio}`);
 
-            const timpoTotalEspera = diferenciaHoras + diferenciaMinutos + diferenciaSegundos;
+            const diferenciaMilisegundos = horaEnvioDATE - horaActualDATE;
 
-            console.log(`Diferencia de Horas en milisegundos: ${diferenciaHoras}`);
-            console.log(`Diferencia de Minutos en milisegundos: ${diferenciaMinutos}`);
-            console.log(`Diferencia de Segundos en milisegundos: ${diferenciaSegundos}`);
-            console.log(`Tiempo Total de Espera: ${timpoTotalEspera}`);
+            console.log(`Diferencia Date Milisegundos: ${diferenciaMilisegundos}`);
 
-            return timpoTotalEspera;
+            return diferenciaMilisegundos;
         }
 
         console.log(`Fecha actual: ${fecha_actual}`);
@@ -42,8 +43,13 @@ class EstadoProgramado {
             try {
 
                 enviarCorreosNodemailer(instanciaCorreo.correo, instanciaCorreo.asunto, instanciaCorreo.mensaje);
+
                 console.log(`Correo programado enviado a ${instanciaCorreo.correo}`);
-                instanciaCorreo.estadoCorreoObjeto = new EstadoEnviado();
+
+                instanciaCorreo.estadoCorreoObjeto = new EstadoEnviado();//Ahora se le asignó un Objeto de la clase EstadoEnviado, por lo que si se usar el método "enviarCorreo" se devolverá un mensaje indicado que ya se envió el correo
+
+                instanciaCorreo.transicionEstadoParticipante(instanciaCorreo.campana_id, 'Enviado');
+                instanciaCorreo.estado = 'Enviado'
 
             } catch(error) {
                 throw console.error('Error al enviar correos con Nodemailer', error.message)
@@ -53,11 +59,10 @@ class EstadoProgramado {
         if(fecha_envio < fecha_actual) {
             enviarCorreoNodemailer();
 
-            // const result = await clienteRepository.modificarEstadoParticipante(instanciaCorreo.campana_id, instanciaCorreo.estado);
-
         } else if(fecha_envio == fecha_actual) {
             if(hora_envio <= hora_actual) {
                 enviarCorreoNodemailer();
+
             } else {
  
                 const tiempoTotalEspera = calcularDiferenciaHora();
@@ -65,16 +70,20 @@ class EstadoProgramado {
                 setTimeout(enviarCorreoNodemailer, tiempoTotalEspera);
 
                 console.log(`El correo programado para ${instanciaCorreo.correo} debe ser enviado hoy a las ${hora_envio}`);
+
             }
         } else {
             console.log(`El correo programado para ${instanciaCorreo.correo} debe ser enviado el ${fecha_envio} a las ${hora_envio}`);
+
+            //ACÁ DEBE IR CÓDIGO PARA QUE EL CORREO SE ENVÍE DÍAS DESPUÉS ->  SE DEBE USAR setTimeout DE NUEVO CREO
         }
+
     }
 }
 
-//Estado Enviado
-class EstadoEnviado {
+class EstadoEnviado extends EstadoCorreo {
     enviarCorreo(instanciaCorreo) {
+        // Realizar acciones específicas del estado enviado
         console.log(`Este correo ya ha sido enviado a ${instanciaCorreo.correo}`);
     }
 }
@@ -95,24 +104,14 @@ class Correo {
         this.correo = correo;//DE AHI SI NECESITO LOS DEMÁS DATOS DE datosUnCliente LOS INICIALIZO EN ESTE CONSTRUCTOR
 
         this.estadoCorreoObjeto = new EstadoProgramado();//Con esto determino el Estado del Correo, en cada CLASE. No es lo mismo que el 'estado' de datosDelCorreo
-
-        // this.datosCliente = [];
-
-        // for(const datosCliente of dni_y_EstadoClientes) {
-
-        //     this.datosCliente.push({
-        //         cliente_id: datosCliente.cliente_id,
-        //         estado: datosCliente.estado //ESTE ESTADO ES EL DEL PARTICIPANTE -> ENVIADO O PROGRAMADO
-        //     })
-        // }
     }
 
-    async transicionEstado(campana_id, estado) {// NO LO ESTOY USANDO EN LA LA CLASE Correo 
-        const result = await clienteRepository.modificarEstadoParticipante(campana_id, estado);
+    async transicionEstadoParticipante(campana_id, estado) {// NO LO ESTOY USANDO EN LA LA CLASE Correo 
+        await clienteRepository.modificarEstadoParticipante(campana_id, estado);
     }
 
     enviar() {
-        this.estadoCorreoObjeto.enviarCorreo(this/*, correo*/);//Al pasarle el this le estoy pasando una instancia de la misma clase Correo
+        this.estadoCorreoObjeto.enviarCorreo(this);//Al pasarle el this le estoy pasando una instancia de la misma clase Correo
     }
 }
 
